4a8d4ddafe995376d4a2320953e1e5ed54cba82c
Charppi <anticharppi@gmail.com>
2024-02-07 21:55:12 -0500

    add drones controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DronesController } from './drones/drones.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, DronesController],
   providers: [AppService],
 })
 export class AppModule {}
