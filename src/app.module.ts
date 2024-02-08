36132943ea590aba43bc193fd536d497ac25c674
Charppi <anticharppi@gmail.com>
2024-02-07 22:05:03 -0500

    Add DronesService to AppModule and create DronesService base unit test

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DronesController } from './drones/drones.controller';
+import { DronesService } from './drones/drones.service';
 
 @Module({
   imports: [],
   controllers: [AppController, DronesController],
-  providers: [AppService],
+  providers: [AppService, DronesService],
 })
 export class AppModule {}
