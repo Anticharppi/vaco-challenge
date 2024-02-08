dd8ca175f2bde9fb9ecb302c4c1dd072111c5208
Charppi <anticharppi@gmail.com>
2024-02-07 22:35:49 -0500

    Add new services and repositories

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DronesController } from './drones/drones.controller';
-import { DronesService } from './drones/drones.service';
+import { CreateDroneService } from './drones/create-dron.service';
+import { DronesRepositoryService } from './repositories/drones-repository/drones-repository.service';
+import { PrismaService } from './repositories/prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController, DronesController],
-  providers: [AppService, DronesService],
+  providers: [
+    AppService,
+    PrismaService,
+    CreateDroneService,
+    DronesRepositoryService,
+  ],
 })
 export class AppModule {}
